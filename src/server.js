68a8ff279f800f04d091cd61af88720d4d2c4c64
Guilherme Vinicius Mello de Lima <guimellolima7715@gmail.com>
2024-02-15 19:42:41 -0300

    ínicio do projeto

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import { Express } from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({message: 'Hello World!'})
+})
+
+app.get('/produto', (req, res) => {
+  res.json({message:'Hello Produto!'})
+})
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta http://localhost: ${port}`)
+})
